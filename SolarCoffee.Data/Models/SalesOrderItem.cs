5d9ccc887d738da0af5898676f26a534857b838f
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2020-05-02 16:53:48 +0100

    Add entity models

---
+++ SolarCoffee.Data/Models/SalesOrderItem.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SolarCoffee.Data.Models
+{
+    public class SalesOrderItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public Product Product { get; set; }
+    }
+}
+
+
